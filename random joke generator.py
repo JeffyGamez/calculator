67b3022fcbd76c3cf8831314412fd54d6c20bf13
JeffyGamez <259977003+JeffyGamez@users.noreply.github.com>
2026-04-22 09:10:32 -0400

    Fix version number and enhance user input handling
    Updated version number and improved input validation.

--- random joke generator.py
+++ random joke generator.py
@@ -4,7 +4,7 @@ import math
 print ("This repo was made by jeffygamez")
 print ("now loading")
 time.sleep (5)
-print ("Welcome To Random Joke Generator version 1.3")
+print ("Welcome To Random Joke Generator version 1.3.2")
 joke_1 = "Why did the chicken cross the road? To run from the KFC"
 joke_2 = "Have you heard the rumor of the margarin? Never mind I butter not spread it"
 joke_3 = "What did the eraser say to the pencil? You're lookin mighty sharp today"
@@ -27,15 +27,21 @@ while keep_playing == "yes" or keep_playing == "Yes":
     elif joke_selected == 3:
       print (f"Random Joke Is: {joke_3}")
    
-  if joke_selected == range 1, 3:  
+  valid_input = False 
+  while valid_input == False:
     
     keep_playing = input ("Would You Like To Keep Playing Yes/No")
   
-  if keep_playing == "Yes" or keep_playing == "yes":
+    if keep_playing == "Yes" or keep_playing == "yes":
+      valid_input = True
       continue
-  elif keep_playing == "No" or keep_playing == "no":
+    elif keep_playing == "No" or keep_playing == "no":
       print ("Goodbye Thank You For Using Random Joke Generator Version 1.3 By JeffyGamez")
+      valid input = True
       break
+    else:
+      print ("ERROR, Invalid Input!")
+      
   
 
 
